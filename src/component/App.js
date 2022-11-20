529cc7cd1e4b8d1c6fcb55711548980c2f0fbe05
seeratbhatia <b.seerat@gmail.com>
2022-11-20 22:54:59 +0530

    Add files via upload

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+import Main from "./Main";
+function App(props) {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./component/App";
+import "./style/style.css";
+
+render(<App />, document.getElementById("root"));
